687e9141d8c2863a015e031ca370171a14eae566
biaIT <tienquynh0401@gmail.com>
2025-05-15 09:09:27 +0700

    video 16

---
+++ src/componets/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MyComponets extends React.Component {
+    render() {
+        return (
+            <div>
+                My first component
+                {Math.random()}
+            </div>
+        );
+    };
+    
+}
+
+export default MyComponets;
